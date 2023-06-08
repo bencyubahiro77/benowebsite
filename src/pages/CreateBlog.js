55b89e9ee42a389767800ad001dbe1bb8db9f9ae
cyubahiro benjamin <bencyubahiro77@gmail.com>
2023-06-08 17:00:11 +0200

    overflow text editor

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -5,7 +5,6 @@ import Swal from 'sweetalert2';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 
-
 const CreateBlogPage = () => {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
@@ -33,42 +32,42 @@ const CreateBlogPage = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
       setPublishing(true);
-  
+
       const token = localStorage.getItem('token');
       const formData = new FormData();
       formData.append('title', title);
       formData.append('desc', content);
       formData.append('category', category);
       formData.append('image', coverPhoto);
-  
+
       // eslint-disable-next-line no-unused-vars
       const response = await axios.post(`${process.env.REACT_APP_BASE_URL}blog`, formData, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-  
+
       Swal.fire({
         icon: 'success',
         title: 'Blog Created Successfully',
-        showConfirmButton:true,
+        showConfirmButton: true,
         timer: 1500,
       });
-  
+
       // Reset form values
       setTitle('');
       setCategory('');
       setCoverPhoto(null);
       setContent('');
-  
+
       // Navigate to /blog
       navigate('/blog');
     } catch (error) {
       console.error('Error creating blog:', error);
-  
+
       Swal.fire({
         icon: 'error',
         title: 'Error creating blog',
@@ -78,7 +77,7 @@ const CreateBlogPage = () => {
       setPublishing(false);
     }
   };
-  
+
   return (
     <div className="create-blog-page">
       <h1>Create a Blog</h1>
@@ -91,11 +90,12 @@ const CreateBlogPage = () => {
             value={title}
             onChange={handleTitleChange}
             placeholder="Enter the title of your blog"
+            required
           />
         </div>
         <div className="form-group">
           <label htmlFor="category">Category:</label>
-          <select id="category" value={category} onChange={handleCategoryChange}>
+          <select id="category" value={category} onChange={handleCategoryChange} required>
             <option value="">Select a category</option>
             <option value="technology">Technology</option>
             <option value="coding">Coding</option>
@@ -105,19 +105,18 @@ const CreateBlogPage = () => {
         </div>
         <div className="form-group">
           <label htmlFor="coverPhoto">Cover Photo:</label>
-          <input
-            id="coverPhoto"
-            type="file"
-            onChange={handleCoverPhotoChange}
-          />
+          <input id="coverPhoto" type="file" onChange={handleCoverPhotoChange} required />
         </div>
         <div className="form-group">
           <label htmlFor="content">Content:</label>
-          <CKEditor
-            editor={ClassicEditor}
-            data={content}
-            onChange={handleContentChange}
-          />
+          <div className="blogEditor">
+            <CKEditor
+              editor={ClassicEditor}
+              data={content}
+              onChange={handleContentChange}
+              required
+            />
+          </div>
         </div>
         <button type="submit" disabled={publishing}>
           {publishing ? 'Publishing...' : 'Publish'}
